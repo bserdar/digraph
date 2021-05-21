25bdba835b9d14c923953cfb13124988cdbd6f70
Burak Serdar <brxd@cloudprivacylabs.com>
2021-05-20 22:17:20 -0600

    Add sinks/sources among nodes

--- algs.go
+++ algs.go
@@ -17,6 +17,19 @@ func Sinks(g *Graph, includeDisconnected bool) []*Node {
 	return ret
 }
 
+// SinksAmongNodes finds the sink nodes among the given nodes
+func SinksAmongNodes(nodes []*Node, includeDisconnected bool) []*Node {
+	ret := make([]*Node, 0)
+	for _, node := range nodes {
+		if !node.AllOutgoingEdges().HasNext() {
+			if includeDisconnected || node.AllIncomingEdges().HasNext() {
+				ret = append(ret, node)
+			}
+		}
+	}
+	return ret
+}
+
 // Sources returns all nodes that have no incoming edges. If
 // includeDisconnected is true, includes nodes that are not connected
 // to the graph
@@ -25,6 +38,20 @@ func Sources(g *Graph, includeDisconnected bool) []*Node {
 	nodes := g.AllNodes()
 	for nodes.HasNext() {
 		node := nodes.Next()
+		if !node.AllIncomingEdges().HasNext() {
+			if includeDisconnected || node.AllOutgoingEdges().HasNext() {
+				ret = append(ret, node)
+			}
+		}
+	}
+	return ret
+}
+
+// SourcesAmongNodes returns all nodes that have no incoming edges
+// among the given nodes.
+func SourcesAmongNodes(nodes []*Node, includeDisconnected bool) []*Node {
+	ret := make([]*Node, 0)
+	for _, node := range nodes {
 		if !node.AllIncomingEdges().HasNext() {
 			if includeDisconnected || node.AllOutgoingEdges().HasNext() {
 				ret = append(ret, node)
